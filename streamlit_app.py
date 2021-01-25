11e26dccb422ea093d5634df409ced9318497402
fhebal <ferdynandhebal@gmail.com>
2021-01-25 10:56:09 -0600

    Updated app to highlight actual features

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,29 +1,39 @@
 import streamlit as st
-
+import yaml
 from load_css import local_css
 
 local_css("style.css")
+
+
+keys = {
+        'Gastroenterology': 0,
+
+        }
+
+option = st.radio("Press the button", [0, 1, 2, 3, 4])
+option = 
 
 
 class Highlighter():
     def __init__(self):
         self.start = "<span class='highlight blue'><span class='bold'>"
         self.end = "</span></span>"
-        self.lookup = ['yo', 'Fanilo']
     def highlight_match(self, text, config):
-        for value in self.lookup:
+        for value in config:
             text = text.replace(value, "{0}"+value+"{1}")
         text = "<div>" + text.format(self.start, self.end) + "</div>"
         return text
 
-import yaml
 
-with open("example.yaml", 'r') as stream:
+label = 1
+with open("config/{}.yaml".format(label), 'r') as stream:
     try:
-        print(yaml.safe_load(stream))
+        config = stream.read().splitlines()
     except yaml.YAMLError as exc:
         print(exc)
 
+
+print(type(config))
 t = """Hello there my name is per second spike and my other name is sharp wave activity"""
 highlighter = Highlighter()
 t = highlighter.highlight_match(t, config)
