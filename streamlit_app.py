f1fed9e34ba47da14220b5719f28c1e720302f45
fhebal <ferdynandhebal@gmail.com>
2021-01-26 15:16:09 -0600

    Cleaned up app and generate predictions

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,8 @@ import tensorflow as tf
 import tensorflow_hub as hub
 import tensorflow_text as text
 import numpy as np
+from random import sample
+import os
 
 local_css("style.css")
 
@@ -16,35 +18,49 @@ prediction_key = {
         4:'Urology'
         }
 
+
 class Highlighter():
     def __init__(self):
         self.start = "<span class='highlight blue'><span class='bold'>"
         self.end = "</span></span>"
     def highlight_match(self, text, config):
         for value in config:
-            text = text.replace(value, "{0}"+value+"{1}")
+            text = text.replace(" "+value+" ", "{0}"+value+"{1}")
         text = "<div>" + text.format(self.start, self.end) + "</div>"
         return text
 
 
 # Load model from file
 model = tf.keras.models.load_model('/home/muody/saved_model/my_model', compile=False)
-#load data
-medical_notes_dir = '/home/muody/data/medicalnotes/dataset/train-data'
-sample_data = tf.keras.preprocessing.text_dataset_from_directory(
-    medical_notes_dir,
-    batch_size=32)
+
+
+# load data
+def load_data():
+    data_path = '/home/muody/data/medicalnotes/dataset/unlabeled-test-data/'
+    files = os.listdir(data_path)
+    sample_file = data_path + sample(files, 1)[0]
+    with open(sample_file, 'r') as stream:
+        sample_data = stream.read()
+        sample_data = sample_data.replace('\n','')
+        sample_data = sample_data.replace('</B>','')
+        sample_data = sample_data.replace('<B>','')
+    return sample_data
+
 
 def main():
     # INPUT DATA
-    #sentence = st.text_input('Input your sentence here:')
-    #st.write(model.predict(sentence))
-    sample = [x.numpy()[0] for x, y in sample_data.take(1)][0].decode('utf-8')
-    sample = sample.replace('\n','').replace('<B>','').replace('</B>','')
-    prediction_num = np.argmax(model.predict([sample]))
+    #sample = st.text_input('Input your sentence here:')
+    sample = load_data()
+    prediction_arr = tf.sigmoid(model.predict(tf.convert_to_tensor([sample]))).numpy()
+    prediction_num = np.argmax(prediction_arr)
     prediction = prediction_key[prediction_num]
 
-    st.write(prediction, prediction_num)
+    prediction_text = "<div>Prediction: <span class='highlight red'><span class='bold'>" + prediction + '</span></span></div>'
+
+    st.markdown(prediction_text, unsafe_allow_html=True)
+    st.write('\n')
+    for key, value in prediction_key.items():
+        st.write(value, prediction_arr[0][key])
     label = prediction_num
     with open("config/{}.yaml".format(label), 'r') as stream:
         try:
@@ -52,14 +68,10 @@ def main():
         except yaml.YAMLError as exc:
             print(exc)
 
-
-    print(type(config))
-    #t = """Hello there my name is per second spike and my other name is sharp wave activity"""
-
     highlighter = Highlighter()
     t = highlighter.highlight_match(sample, config)
     st.markdown(t, unsafe_allow_html=True)
 
 
-if st.button("Click"):
+if st.button("New Text Sample"):
     main()
